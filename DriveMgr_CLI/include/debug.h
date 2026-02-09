a9d154fc1e3643391b36a027330df67565801585
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2026-02-09 16:53:38 +0100

    Add files via upload

---
+++ DriveMgr_CLI/include/debug.h
@@ -0,0 +1,18 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <iostream>
+
+
+/**
+ * @brief debug message for eysier debugging
+ * @param message contains the message
+ * @param is_active if true then debug message will be printed
+ */
+int debug_msg(const std::string& message, bool is_active);
+
+
+
+
+
+#endif 
