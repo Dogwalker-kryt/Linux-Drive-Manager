4ce8a8375a694ed1ae25ae7c7bb64abbd6c48d7e
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2026-04-17 18:23:58 +0200

    Update DriveMgr_experi.cpp

--- DriveMgr_CLI/src/DriveMgr_experi.cpp
+++ DriveMgr_CLI/src/DriveMgr_experi.cpp
@@ -19,7 +19,7 @@
 // ! Warning this version is the experimental version of the program,
 // This version has the latest and newest functions, but may contain bugs and errors
 // Current version of this code is in the VERSION macro below and in the line bellow
-// v0.9.24.59_dev
+// v0.9.24.60_dev
 
 // C++ libraries
 #include <regex>
@@ -40,7 +40,7 @@
 // ==================== global variables and definitions ====================
 
 // === Version ===
-#define VERSION std::string("v0.9.24.59_dev")
+#define VERSION std::string("v0.9.24.60_dev")
 
 // === globals ===
 struct termios oldt, newt;
